e4ac5303a0e6681d182fda7351f238a31f3911a7
cdessanti <candido.dessanti@hotmail.it>
2023-12-11 09:30:46 +0100

    Rebranded the connecto from Omnisci to HJEAVY.AI. - Updated the download Link to support portal - Added support for the following string functions: CONTAINS STARTSWITH ENDSWITH FILTER_CONTAINS FILTER_STARTSWITH FILTER_ENDSWITH FIND FIND FIND LEN LOWER UPPER PROPER REPEAT SPACE SPLIT REPLACE LEFT LEFT RIGHT RIGHT MID MID MID MID MID MID TRIM LTRIM RTRIM MIN MAX
    Added support for those extended and custom string functions
    
    REGEXP_MATCH
    GET_JSON_OBJECT
    
    KEY_FOR_STRING
    JSON_VALUE
    URL_ENCODE
    URL_DECODE
    BASE64_ENCODE
    BASE64_DECODE
    ENCODE_TEXT
    HASH
    JAROWINKLER_SIMILARITY

--- omnisci_jdbc/connectionBuilder.js
+++
@@ -1,5 +0,0 @@
-(function dsbuilder(attr) {
-    var urlBuilder = "jdbc:omnisci:" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + ":" + attr[connectionHelper.attributeDatabase];
-
-    return [urlBuilder];
-})
