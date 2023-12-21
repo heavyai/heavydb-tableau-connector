e9b748f42f0ede661b57c4f6cf208b6e466b3c37
cdessanti <candido.dessanti@hotmail.it>
2023-12-21 08:53:09 +0100

    Added a basic bash shell to setup the environment needed to connector packager and package the connector

---
+++ utils/packager.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if conda env list | grep tacosigner
+then
+    conda activate tacosigner
+else
+    conda create -y -n tacosigner python=3.10
+    conda activate tacosigner
+fi;
+
+if ! [ -d connector-plugin-sdk ]; then
+    git clone https://github.com/tableau/connector-plugin-sdk.git
+fi
+
+cd connector-plugin-sdk/connector-packager 
+python setup.py install
+
+python -m connector_packager.package ../../../heavyai_jdbc/
