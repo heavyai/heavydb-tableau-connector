f32d268a3ec8100de3e3e0fe0a77f66450f392a7
misiugodfrey <misiug@nvidia.com>
2026-06-10 16:24:39 -0700

    chore: add NVIDIA SPDX headers to 4 source files
    Directly inserts NVIDIA SPDX license headers into files that did not
    receive them in the previous commit (verify-copyright only updates files
    that already have some form of copyright header). Part of org-wide SPDX
    header rollout (see heavydb-internal#8287).

--- heavyai_jdbc/connectionBuilder.js
+++ heavyai_jdbc/connectionBuilder.js
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+// SPDX-License-Identifier: Apache-2.0
+
 (function dsbuilder(attr) {
     var urlBuilder = "jdbc:heavyai:" + attr[connectionHelper.attributeServer] + ":" + attr[connectionHelper.attributePort] + ":" + attr[connectionHelper.attributeDatabase];
 
